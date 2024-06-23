254a7a4c970bbb621368b2a5a19d95fedc9a115b
Cheastear <mr.cheesteer@gmail.com>
2024-06-23 15:55:12 +0300

    done

---
+++ src/components/Line.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+
+function Line({ data, onChange, index = 0, Name = "" }) {
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    setSum(
+      data.reduce((value, current) => {
+        return value + current;
+      }, 0)
+    );
+  }, [data]);
+
+  return (
+    <tr>
+      <th>{Name != "" ? Name : index + 1}</th>
+      <th>{sum}</th>
+      {data.map((elem, i) => {
+        return (
+          <th key={i}>
+            <input
+              value={elem}
+              type="text"
+              onChange={(e) => onChange(index, i, parseInt(e.target.value))}
+            />
+          </th>
+        );
+      })}
+    </tr>
+  );
+}
+
+Line.propTypes = {
+  data: PropTypes.array,
+  index: PropTypes.number,
+  onChange: PropTypes.func,
+  Name: PropTypes.string,
+};
+
+export default Line;
